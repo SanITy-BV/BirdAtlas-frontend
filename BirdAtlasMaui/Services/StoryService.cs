a9ecf083fb053657219c1a811baaefda0310e2ff
Depechie <glenn.versweyveld@gmail.com>
2021-08-17 20:01:21 +0200

    Maui app
    uses Refit to connect to backend API

---
+++ BirdAtlasMaui/Services/StoryService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BirdAtlasMaui.API.Client;
+using BirdAtlasMaui.API.Models;
+
+namespace BirdAtlasMaui.API.Services
+{
+    public class StoryService : IStoryApi
+    {
+        private readonly IStoryApi _storyApi;
+
+        public StoryService()
+        {
+            _storyApi = RestServiceFactory.CreateService<IStoryApi>();
+        }
+
+        public Task<List<Story>> Stories(int page = 0, int amount = 10)
+        {
+            return _storyApi.Stories(page, amount);
+        }
+
+        public Task<List<Story>> FeaturedStories(int amount = 10)
+        {
+            return _storyApi.FeaturedStories(amount);
+        }
+    }
+}
