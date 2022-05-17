98a45ea4c7b3eb69eda6655f7b09a84826532e76
Depechie <glenn.versweyveld@gmail.com>
2022-05-17 22:34:37 +0200

    Added Azure information

--- BirdAtlasAngular/src/environments/environment.prod.ts
+++ BirdAtlasAngular/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
   ProjectName: 'BirdAtlas frontend',
+  msalAppClientId: '5fba7ab6-6f47-43a6-aa23-a7723725b461',
+  msalTenantId: '5f672caa-bb37-4efe-906d-484716e32f15',
+  redirectUrl: 'http://localhost:4200',
+  apiUrl: 'https://dbrd-appkhecyjjr7t4z2.azurewebsites.net/atlas',
+  apiAppIdUri: 'api://907b7b68-e7b0-45e2-bd47-7805a8f09a3a/api.read'
 };
